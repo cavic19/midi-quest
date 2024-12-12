3202a52ee2325624929a3e27b46f7771f52c65a2
David Tomecek <david.tomecek1@seznam.cz>
2024-12-12 14:37:24 +0100

    On succes and failure flash the score with color

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -13,7 +13,10 @@ import ChordGenerator from "./models/ChordGenerator";
 import ChordDefinition from "./models/ChordDefinition";
 import KeyboardShape from "./models/KeyboardShape";
 import NoteGenerator from "./models/NoteGenerator";
+import classNames from "classnames";
 
+// Should be lower than human reaction time < 150ms
+const SCORE_FLASH_DURATION = 100
 
 function App() {
   const { current: noteGenerator } = useRef<KeyboardShapeGenerator>(new NoteGenerator());
@@ -22,51 +25,68 @@ function App() {
   const [shape, setShape] = useState<KeyboardShape>(() =>  noteGenerator.next());
   const noteNotation = useMemo(() => shape.name(), [shape]);
   const maxTime = generateTime(score);
-  const [gameStarted, setGameStarted] = useState(false);
+  const [isGameRunning, setIsGameRunning] = useState(false);
   const [iseGameOver, setIsGameOver] = useState(false);
   const { timeLeft, restart: restartTimer, stop: stopTimer } = useTimer(maxTime);
+  // Used to control flash status of the score.
+  const [flashSuccess, setFlashSuccess] = useState(false);
+  const [flashFailure, setFlashFailure] = useState(false);
 
   useEffect(() => {
-    if (gameStarted) {
+    if (isGameRunning) {
       setScore(0);
       restartTimer();
     } else {
       stopTimer();
     }
-  }, [gameStarted])
+  }, [isGameRunning])
 
   useEffect(() => {
     if (timeLeft == 0) {
-      restartTimer();
-      setLives(ls => ls - 1);
-      setShape(noteGenerator.next())
+      setFlashFailure(true);
+      // We have to set timeout otherwise, the next shape would get colored
+      setTimeout(() => {
+        setFlashFailure(false)
+        restartTimer();
+        setLives(ls => ls - 1);
+        setShape(noteGenerator.next())
+      } , SCORE_FLASH_DURATION)
     }
   }, [timeLeft]);
 
   useEffect(() => {
     if (lives <= 0) {
       setIsGameOver(true);
-      setGameStarted(false);
+      setIsGameRunning(false);
     }
   }, [lives]);
 
 
   const handleTilesChange = useCallback((tiles: Note[], event: PressedTilesChangeEvent) => {
-    if (gameStarted && event === "PRESSED") {
+    if (isGameRunning && event === "PRESSED") {
       if (!shape.includesNotes(tiles)) {
         // Player pressed extra notes => fail
-        setLives(lives => lives - 1)
+        setFlashFailure(true);
+        setTimeout(() => {
+          setFlashFailure(false)
+          setLives(lives => lives - 1)
+        }, SCORE_FLASH_DURATION)
+
       } else if (shape.equalTo(tiles)) {
-        setShape(noteGenerator.next());
-        setScore(sc => sc + 1);
-        restartTimer();
+        setFlashSuccess(true);
+        setTimeout(() =>  {
+          setFlashSuccess(false);
+          setShape(noteGenerator.next());
+          setScore(sc => sc + 1);
+          restartTimer();
+        }, SCORE_FLASH_DURATION)
       } else {
         // This means that user pressed onlu some of the notes making up the shape
         // E.g. C - E but hasn't pressed yet G to create C major chord, so he still has a chance
       }
     }
 
-  }, [shape, gameStarted]);
+  }, [shape, isGameRunning]);
 
   return (
     // This should be in body...
@@ -78,16 +98,16 @@ function App() {
               setIsGameOver(false);
               setLives(MAX_HEALTH);
               setScore(0);
-              setGameStarted(true);
+              setIsGameRunning(true);
             }} />
-          ) : gameStarted ? (
-            <span className="text-white text-6xl">{noteNotation}</span>
+          ) : isGameRunning ? (
+            <span className={classNames("text-6xl", { "text-[#FF3737]": flashFailure, "text-[#6EF2B0]": flashSuccess, "text-white": !flashSuccess && !flashFailure })}>{noteNotation}</span>
           ) : (
-            <NewGameScreen onGameStart={() => setGameStarted(true)} />
+            <NewGameScreen onGameStart={() => setIsGameRunning(true)} />
           )}
         </div>
         <PianoBoard hud={
-          gameStarted && (
+          isGameRunning && (
             <div className="mb-4 flex flex-col gap-4">
               <div className="flex items-center justify-between">
                 <HealthBar maxHealth={MAX_HEALTH} health={lives} />
